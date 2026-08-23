8110798d3b748f9ebc15a1606e05edd7555d020f
lschwe04 <Lukas_Schweiger@outlook.de>
2026-08-23 18:05:09 +0200

    1

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -3,11 +3,12 @@ provider "hcloud" {
 }
 
 resource "hcloud_server" "soc_hub" {
-  name        = var.server_name
-  image       = "rocky-9"
-  server_type = var.server_type
-  location    = "fsn1"
-  ssh_keys    = [var.ssh_key_name]
+  name         = var.server_name
+  image        = "rocky-9"
+  server_type  = var.server_type
+  location     = "fsn1"
+  ssh_keys     = [var.ssh_key_name]
+  firewall_ids = [hcloud_firewall.soc_hub_fw.id] # NEU: Firewall zugewiesen
 
   public_net {
     ipv4_enabled = true
